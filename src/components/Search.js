b1f56237e3cf88ae5365066079d9b6d4c09fc574
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-28 22:51:47 +0530

    initial list error

--- src/components/Search.js
+++ src/components/Search.js
@@ -21,15 +21,19 @@ const Search = () => {
             setResults(data.query.search);
         };
 
-        const timeoutId = setTimeout(() => {
-            if (term) {
+        if (term && !results.length) {
+            search();
+          } else {
+            const timeoutId = setTimeout(() => {
+              if (term) {
                 search();
-            }
-        }, 1000);
-        return () => {
-            clearTimeout(timeoutId);
-        };
-
+              }
+            }, 1000);
+      
+            return () => {
+              clearTimeout(timeoutId);
+            };
+          }
 
     }, [term])
     const renderedResult = results.map((result) => {
