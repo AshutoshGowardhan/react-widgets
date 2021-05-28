41c5294c9b9bc44eebfa43a890a86fe2be5192da
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-28 15:59:40 +0530

    using state in functional component, descructuring

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Accordian = ({items}) => {
-
+const Accordian = ({ items }) => {
+    const [actveIndex, setActiveIndex] = useState(null);
     const onTitleClicked = (index) => {
-        console.log('title clicked',index)
+        setActiveIndex(index);
     }
-    const renderedItems = items.map((item,index) => {
+    const renderedItems = items.map((item, index) => {
         return <React.Fragment key={item.title}>
-            <div 
-            className="title active"
-            onClick={()=>onTitleClicked(index)}>
+            <div
+                className="title active"
+                onClick={() => onTitleClicked(index)}>
                 <i className="dropdown icon"></i>
                 {item.title}
             </div>
@@ -21,6 +21,7 @@ const Accordian = ({items}) => {
     return (
         <div className="ui styled accordion">
             {renderedItems}
+            <h1>{actveIndex}</h1>
         </div>
     )
 }
