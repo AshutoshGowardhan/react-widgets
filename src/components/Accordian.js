02d0645cdcfcc88ab1d80fe24db1b00a26157431
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-28 15:48:04 +0530

    active state,index

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const Accordian = ({items}) => {
-    const renderedItems = items.map(item => {
+
+    const onTitleClicked = (index) => {
+        console.log('title clicked',index)
+    }
+    const renderedItems = items.map((item,index) => {
         return <React.Fragment key={item.title}>
-            <div className="title active">
+            <div 
+            className="title active"
+            onClick={()=>onTitleClicked(index)}>
                 <i className="dropdown icon"></i>
                 {item.title}
             </div>
