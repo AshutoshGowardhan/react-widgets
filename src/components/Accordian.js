ad2cc6d2f40f7ebad1553a97d4e6f088ffcea691
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-28 15:23:58 +0530

    creating the list and passing at as prop

---
+++ src/components/Accordian.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Accordian = ({items}) => {
+    return (
+        <div>
+            <h1>{items.length}</h1>
+        </div>
+    )
+}
+
+
+export default Accordian;
---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Accordian from './Accordian'
+
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favourite JS library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use react creating components'
+    }
+]
+
+export default () => {
+    return (
+        <Accordian items={items}/>
+    )
+}
