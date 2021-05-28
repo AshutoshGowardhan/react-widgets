58e5c4940e8d1ddc7c65766da598b8da9225f60f
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-28 18:00:55 +0530

    solving async error in useEffect Hook

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search = () => {
     const [term,setTerm] = useState('');
     useEffect(()=>{
-        console.log(term);
+        const searchResult = async () => {
+            await axios.get();
+        }
     },[term])
     return (
         <div>
