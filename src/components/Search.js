8aeec28823b8d3d6bf335c206ef105eaf107cf7c
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-28 18:13:24 +0530

    feting data setting the state

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,10 +2,12 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const Search = () => {
-    const [term,setTerm] = useState('');
+    const [term,setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
+    console.log(results)
     useEffect(()=>{
         const search = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php', {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
@@ -14,8 +16,12 @@ const Search = () => {
                     srsearch: term,
                 }
             });
+            setResults(data.query.search);
         }
-        search();
+        if(term) {
+            search();
+        }
+        
     },[term])
     return (
         <div>
