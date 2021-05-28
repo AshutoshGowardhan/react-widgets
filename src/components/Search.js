70909003c3704fe38a46c6701ca9078f0f710e30
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-28 18:06:44 +0530

    fetching data from wikipedia api

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,9 +4,18 @@ import axios from 'axios';
 const Search = () => {
     const [term,setTerm] = useState('');
     useEffect(()=>{
-        const searchResult = async () => {
-            await axios.get();
+        const search = async () => {
+            await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+            });
         }
+        search();
     },[term])
     return (
         <div>
