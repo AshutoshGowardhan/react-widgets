325f47d7d48ce32ae95661c3c2b2bbdd5c157388
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-28 19:19:01 +0530

    linking to wikipedia page

--- src/components/Search.js
+++ src/components/Search.js
@@ -25,7 +25,13 @@ const Search = () => {
     },[term])
     const renderedResult = results.map((result)=> {
         return (
-            <div key={result.pageId} className="item">
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a 
+                    className="ui button"
+                    href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >Go</a>
+                </div>
                 <div className="content">
                     <div className="header">
                         {result.title}
