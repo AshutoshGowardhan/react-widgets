13ae3558fb91fac15f3d203cff34c288dadd631b
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-29 23:56:37 +0530

    useRef hook, contains dom

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,28 +1,38 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
     const [open, setOpen] = useState(false);
-
+    const ref = useRef();
     useEffect(() => {
-        document.body.addEventListener('click', () => {
+        document.body.addEventListener(
+          "click",
+          (event) => {
+            if (ref.current.contains(event.target)) {
+              return;
+            }
+    
             setOpen(false);
-        });
-    }, []);
+          },
+          { capture: true }
+        );
+      }, []);
 
-    const renderedOptions = options.map((options)=> {
+    const renderedOptions = options.map((option)=> {
         if(options.value === selected.value) {
             return null;
         }
         return (
             <div 
-            onClick={()=>{(onSelectedChange(options))}}
-            key={options.value} className='item'>
-                {options.label}
+            onClick={() => onSelectedChange(option)}
+            key={option.value} 
+            className='item'>
+                {option.label}
             </div>
         );
     })
+
     return (
-        <div className="ui form">
+        <div className="ui form" ref={ref}>
             <div className="field">
                 <label className="label">Select a color</label>
                 <div 
