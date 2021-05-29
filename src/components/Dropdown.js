b789586b0f49166022e5b6996cadfe07bab733db
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-29 23:42:48 +0530

    eventlistener bubbling

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,14 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
     const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        document.body.addEventListener('click', () => {
+            setOpen(false);
+        });
+    }, []);
+
     const renderedOptions = options.map((options)=> {
         if(options.value === selected.value) {
             return null;
         }
         return (
             <div 
-            onClick={()=>onSelectedChange(options)}
+            onClick={()=>{(onSelectedChange(options))}}
             key={options.value} className='item'>
                 {options.label}
             </div>
@@ -19,7 +26,7 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
             <div className="field">
                 <label className="label">Select a color</label>
                 <div 
-                onClick={()=> setOpen(!open)}
+                onClick={()=> {setOpen(!open)}}
                 className={`ui selection dropdown ${open ? "visible active" : ''}`}>
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
